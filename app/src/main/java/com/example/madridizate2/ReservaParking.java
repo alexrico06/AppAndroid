ef4473b7603294bf87f95899a745fab34dc2148a
leo-cardozo <73230975+leo-cardozo@users.noreply.github.com>
2021-05-26 21:16:57 +0200

    Data Picker y Time Picker

--- app/src/main/java/com/example/madridizate2/ReservaParking.java
+++ app/src/main/java/com/example/madridizate2/ReservaParking.java
@@ -2,28 +2,50 @@ package com.example.madridizate2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.NumberPicker;
 import android.widget.Spinner;
 import android.widget.TimePicker;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
-public class ReservaParking extends AppCompatActivity implements AdapterView.OnItemSelectedListener  {
+public class ReservaParking extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener  {
 
-    TimePicker timePicker;
-    int TIME_PICKER_INTERVAL = 15;
-    NumberPicker minutePicker;
-    List<String> displayedValues;
+
     String direccion;
     Spinner spinner_alias;
+
+    private static final String CERO = "0";
+    private static final String DOS_PUNTOS = ":";
+    private static final String BARRA = "/";
+
+    //Calendario para obtener fecha & hora
+    public final Calendar c = Calendar.getInstance();
+
+    //Fecha
+    final int mes = c.get(Calendar.MONTH);
+    final int dia = c.get(Calendar.DAY_OF_MONTH);
+    final int anio = c.get(Calendar.YEAR);
+
+    //Hora
+    final int hora = c.get(Calendar.HOUR_OF_DAY);
+    final int minuto = c.get(Calendar.MINUTE);
+
+    //Widgets
+    EditText etFecha, etHora;
+    ImageButton ibObtenerFecha, ibObtenerHora;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,17 +53,17 @@ public class ReservaParking extends AppCompatActivity implements AdapterView.OnI
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reserva_parking);
 
-        /*onCreate(savedInstanceState);
-        timePicker = (TimePicker)findViewById(R.id.timePicker1);
-        timePicker.setIs24HourView(true);
-        timePicker.setCurrentHour(0);
-        timePicker.setCurrentMinute(0);
-        setTimePickerInterval(timePicker);
-
-         */
-
         Intent intent = getIntent();
         direccion = intent.getStringExtra("direccion");
+
+        etFecha = (EditText) findViewById(R.id.et_mostrar_fecha_picker);
+        etHora = (EditText) findViewById(R.id.et_mostrar_hora_picker);
+
+        ibObtenerFecha = (ImageButton) findViewById(R.id.ib_obtener_fecha);
+        ibObtenerHora = (ImageButton) findViewById(R.id.ib_obtener_hora);
+
+        ibObtenerFecha.setOnClickListener(this);
+        ibObtenerHora.setOnClickListener(this);
 
         EditText direccionText = findViewById(R.id.direccionParking);
         direccionText.setEnabled(false);
@@ -106,29 +128,64 @@ public class ReservaParking extends AppCompatActivity implements AdapterView.OnI
 
     }
 
+    @Override
+    public void onClick(View v) {
 
-/*
-    private void setTimePickerInterval(TimePicker timePicker) {
-        try {
-            Class<?> classForid = Class.forName("com.android.internal.R$id");
-
-            Field field = classForid.getField("minute");
-            minutePicker = (NumberPicker) timePicker .findViewById(field.getInt(null));
-            minutePicker.setMinValue(0); minutePicker.setMaxValue(7);
-            displayedValues = new ArrayList<String>();
-
-            for (int i = 0; i < 60; i += TIME_PICKER_INTERVAL) {
-                displayedValues.add(String.format("%02d", i));
-            }
-            for (int i = 0; i < 60; i += TIME_PICKER_INTERVAL) {
-                displayedValues.add(String.format("%02d", i));
-            }
-            minutePicker.setDisplayedValues(displayedValues .toArray(new String[0]));
+        switch (v.getId()){
+            case R.id.ib_obtener_fecha:
+                obtenerFecha();
+                break;
+            case R.id.ib_obtener_hora:
+                obtenerHora();
+                break;
         }
-        catch (Exception e) { e.printStackTrace(); }
+
     }
- */
+
+    private void obtenerFecha(){
+        DatePickerDialog recogerFecha = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                final int mesActual = month + 1;
+
+                String diaFormateado = (dayOfMonth < 10)? CERO + String.valueOf(dayOfMonth):String.valueOf(dayOfMonth);
+                String mesFormateado = (mesActual < 10)? CERO + String.valueOf(mesActual):String.valueOf(mesActual);
+
+                etFecha.setText(diaFormateado + BARRA + mesFormateado + BARRA + year);
 
 
+            }
+        },anio, mes, dia);
 
+        recogerFecha.show();
+
+    }
+
+
+    private void obtenerHora(){
+        TimePickerDialog recogerHora = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                //Formateo el hora obtenido: antepone el 0 si son menores de 10
+                String horaFormateada =  (hourOfDay < 10)? String.valueOf(CERO + hourOfDay) : String.valueOf(hourOfDay);
+                //Formateo el minuto obtenido: antepone el 0 si son menores de 10
+                String minutoFormateado = (minute < 10)? String.valueOf(CERO + minute):String.valueOf(minute);
+                //Obtengo el valor a.m. o p.m., dependiendo de la selección del usuario
+                String AM_PM;
+                if(hourOfDay < 12) {
+                    AM_PM = "a.m.";
+                } else {
+                    AM_PM = "p.m.";
+                }
+                //Muestro la hora con el formato deseado
+                etHora.setText(horaFormateada + DOS_PUNTOS + minutoFormateado + " " + AM_PM);
+            }
+            //Estos valores deben ir en ese orden
+            //Al colocar en false se muestra en formato 12 horas y true en formato 24 horas
+            //Pero el sistema devuelve la hora en formato 24 horas
+        }, hora, minuto, false);
+
+        recogerHora.show();
+    }
 }
