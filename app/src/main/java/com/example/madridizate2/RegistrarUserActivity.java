2e6c1afdfb700c09fadbfd35b0cae6eb7734908d
alexrico06 <81771826+alexrico06@users.noreply.github.com>
2021-06-01 23:54:19 +0200

    pordefecto3.0
    //DISEÑO INSERCCION USUARIOS

--- app/src/main/java/com/example/madridizate2/RegistrarUserActivity.java
+++ app/src/main/java/com/example/madridizate2/RegistrarUserActivity.java
@@ -1,20 +1,26 @@
 package com.example.madridizate2;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegistrarUserActivity extends AppCompatActivity {
 
     static String[] datosP = new String[7];
     String[] datosD = new String[6];
+    boolean vacios=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,32 +31,43 @@ public class RegistrarUserActivity extends AppCompatActivity {
 
     public void registrarCoches(View view) {
 
-        insertarUsuario();
+        vacios = insertarUsuario();
 
-        Intent i = new Intent(this, RegistrarVehiculoActivity.class);
-        i.putExtra("registro","A");
-        startActivity(i);
+        if(!vacios){
+            Intent i = new Intent(this, RegistrarVehiculoActivity.class);
+            i.putExtra("registro","A");
+            startActivity(i);
+        }/*else{
+            Toast.makeText(this,"ERROR AL INSERTAR",Toast.LENGTH_SHORT).show();
+        }*/
+
     }
 
     public void pulsarMasTarde(View view) {
 
-        insertarUsuario();
+        vacios = insertarUsuario();
 
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
+        if(!vacios){
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        }/*else{
+            Toast.makeText(this,"ERROR AL INSERTAR",Toast.LENGTH_SHORT).show();
+        }*/
+
+
     }
 
-    private void insertarUsuario(){
+    private boolean insertarUsuario(){
 
         EditText dni = findViewById(R.id.textDNI);
-        datosP[0] = dni.getText().toString();
+        datosP[0] = dni.getText().toString().toUpperCase();
         User.setDni(datosP[0]);
 
         EditText nombre = findViewById(R.id.textNombre);
-        datosP[1] = nombre.getText().toString();
+        datosP[1] = nombre.getText().toString().toUpperCase();
 
         EditText apellidos = findViewById(R.id.textApellidos);
-        datosP[2] = apellidos.getText().toString();
+        datosP[2] = apellidos.getText().toString().toUpperCase();
 
         EditText telefone =  findViewById(R.id.textPhone);
         datosP[3] = telefone.getText().toString();
@@ -85,14 +102,80 @@ public class RegistrarUserActivity extends AppCompatActivity {
         EditText ciudad =  findViewById(R.id.textCiudad);
         datosD[5] = ciudad.getText().toString();
 
-        HiloCliente hilo = new HiloCliente(2, datosP,datosD);
-        hilo.start();
+        Pattern pattern = Pattern.compile ("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"+"[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
 
-        try {
-            hilo.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        Matcher mather = pattern.matcher(datosP[4]);
+
+        if(datosP[0].length()==9){
+            if(!datosP[1].equals("")){
+                if(!datosP[2].equals("")){
+                    if(!datosP[5].equals("")){
+                        if(!datosP[3].equals("")){
+                            if(!datosP[4].equals("")){
+                                if(!datosP[6].equals("")){
+
+
+                                }else{
+                                    //Toast.makeText(this,"INTRODUZCA UNA CONTRASEÑA",Toast.LENGTH_SHORT).show();
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                                    builder.setMessage("CONTRASEÑA VACÍA, DESEA CONTINUAR?");
+
+                                    builder.setPositiveButton("CONTINUAR", new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int id) {
+                                            vacios=false;
+                                        }
+                                    });
+                                    builder.setNegativeButton("CANCELAR", new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int id) {
+                                           dialog.cancel();
+                                        }
+                                    });
+
+                                    AlertDialog dialog = builder.create();
+                                    dialog.show();
+                                }
+                            }else{
+                                Toast.makeText(this,"INTRODUZCA UN CORREO",Toast.LENGTH_SHORT).show();
+                            }
+                        }else{
+                            Toast.makeText(this,"INTRODUZCA UN TELEFONO",Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(this,"INTRODUZCA SU FEHCA DE NACIMIENTO",Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(this,"INTRODUZCA SUS APELLIDOS",Toast.LENGTH_SHORT).show();
+                }
+            }else{
+                Toast.makeText(this,"INTRODUZCA SU NOMBRE",Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(this,"MAL FORMATO DEL DNI",Toast.LENGTH_SHORT).show();
         }
+        /*
+        for (int i=0; datosD.length >= i;i++){
+            System.out.println("datosP[i]");
+            if(datosD[i].equals("")){
+                Toast.makeText(this,"INTRODUZCA TODOS LOS DATOS DE DIRECCION",Toast.LENGTH_SHORT).show();
+                vacios=true;
+                break;
+            }
+        }*/
+
+        System.out.println(vacios);
+
+        if(!vacios){
+            HiloCliente hilo = new HiloCliente(2, datosP,datosD);
+            hilo.start();
+
+            try {
+                hilo.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return vacios;
     }
 
 }
