01235d8ce6285e7d1c415e76e6cc1ce0ec3bf34d
leo-cardozo <73230975+leo-cardozo@users.noreply.github.com>
2021-06-19 14:06:20 +0200

    Cacelar gestion reserva

--- app/src/main/java/com/example/Madridizate/GestionReservas.java
+++ app/src/main/java/com/example/Madridizate/GestionReservas.java
@@ -5,7 +5,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -17,6 +20,8 @@ public class GestionReservas extends AppCompatActivity {
     private TextView plaza;
     private TextView fechaReserva,horas;
     private TextView matricula;
+    private Button button;
+    String[] plazas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +33,7 @@ public class GestionReservas extends AppCompatActivity {
         horas  = findViewById(R.id.horasReserva);
         matricula  = findViewById(R.id.matriculaReserva);
         fechaReserva = findViewById(R.id.fechaReserva);
+        button = findViewById(R.id.cancelarReserva);
 
         HiloCliente hilo = new HiloCliente(16,User.getEmail());
         hilo.start();
@@ -38,14 +44,35 @@ public class GestionReservas extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        for (String[] plazas: hilo.listaReservas) {
+            plazas= hilo.listaReservas;
+
             System.out.println("id: "+ plazas[0] +" dia: "+plazas[1]);
             direccion.setText(plazas[6]);
             fechaReserva.setText(plazas[1].substring(0,10));
             horas.setText(plazas[2]+"-"+plazas[3]);
             plaza.setText(plazas[5]);
             matricula.setText(plazas[4]);
+    }
+
+    public void cancelarReserva(View v) {
+        direccion.setText("");
+        fechaReserva.setText("");
+        horas.setText("");
+        plaza.setText("");
+        matricula.setText("");
+
+        HiloCliente hilo = new HiloCliente(18, plazas[0]);
+        hilo.start();
+
+        try {
+            hilo.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
+
+        Toast.makeText(this,"RESERVA ELIMINADA",Toast.LENGTH_SHORT).show();
+        button.setEnabled(false);
 
     }
 }
